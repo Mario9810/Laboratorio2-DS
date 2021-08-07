e056379706ed43a3e70f9302148b62d70aab99d6
Mario9810 <32363631+Mario9810@users.noreply.github.com>
2021-08-07 01:46:10 -0600

    regular finished

--- regular.R
+++ regular.R
@@ -1,9 +1,13 @@
-install.packages("httpuv")
+install.packages("devtools")
+devtools::install_github("facebook/prophet", subdir='R')
+
+
 library(xts)
 library(tseries)
 library(forecast)
 library(ggfortify)
-library(prophet)
+library(lmtest)
+library(fUnitRoots)
 
 regularC <- read.csv("seriec.csv")
 regularI <- read.csv("seriei.csv")
@@ -39,11 +43,20 @@ ts.plot(regC.ts,2.718^pred$pred, log = "y", lty = c(1,3))
 
 fit2 <- arima(log(regC.ts), c(2, 1, 1),seasonal = list(order = c(0, 1, 0), period = 12))
 
-forecastAP <- forecast(fit, level = c(95), h = 120)
-forecastAD <- forecast(fit2, level = c(95), h = 120)
+forecastAP <- forecast(fit, level = c(95), h = 5)
+forecastAD <- forecast(fit2, level = c(95), h = 5)
+
+#analisis residuales
+checkresiduals(fit)
+checkresiduals(fit2)
+
+coeftest(fit)
+
+qqline(fit$residuals)
+checkresiduals(fit)
 
 autoplot(forecastAP)
-autoplot(forecastAD) #fue mejor el segundo modelo
+autoplot(forecastAD)
 #--------------------------------
 #regular importacion 
 
@@ -63,3 +76,39 @@ logRegulari<- log(regI.ts)
 plot(logRegulari)
 
 acf(logRegulari) #autocorrelacion
+acf(diff(logRegularc),12)#autocorrelacion corregida
+pacf(diff(logRegularc))
+
+adf.test(regI.ts) #test ADF
+
+auto.arima(regI.ts)#obtecion del modelo
+
+fiti <- arima(log(regI.ts), c(1, 1, 2),seasonal = list(order = c(2, 0, 0), period = 12)) #modleo arima con valores dados por la funcion auto.arima()
+pred <- predict(fit, n.ahead = 10*12)
+ts.plot(regI.ts,2.718^pred$pred, log = "y", lty = c(1,3))
+
+fit2i <- arima(log(regI.ts),seasonal = list(order = c(0, 1, 0), period = 12))
+
+#revision de residuales
+checkresiduals(fiti)
+checkresiduals(fit2i)
+
+coeftest(fiti)
+
+qqline(fit$residuals)
+checkresiduals(fiti)
+
+autoplot(forecastAP)
+
+
+
+
+
+forecastAP <- forecast(fit, level = c(95), h = 36)
+forecastAD <- forecast(fit2, level = c(95), h = 36)# a 3 años
+
+forecastAP <- forecast(fit, level = c(95), h = 6)# a 6 meses 
+forecastAD <- forecast(fit2, level = c(95), h = 6)
+
+autoplot(forecastAP)
+autoplot(forecastAD) #fue mejor el primer modelo modelo
